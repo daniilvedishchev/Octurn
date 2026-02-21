383005c5f8ef7818bee2c26c5b1ffda436e8173e
daniilvedishchev <daniil.vedishchev@gmail.com>
2026-02-22 00:33:37 +0100

    configTemplates

---
+++ config/configStruc.hpp
@@ -0,0 +1,22 @@
+/**
+ * @brief This file contains trade configuration all parameters below are available for usage
+ * @details
+ *      - positionSize depends on chosen mode either % of equity or fixed sum
+ *      - comissionBps - buy/sell broker commission
+ *      - slippageBps - buy/sell slippage from orderbook
+ *      - positionMode - fixed or equity percentage 
+*/
+
+enum class Mode {
+    fixed,
+    percent
+};
+
+struct configStructure{
+    double equity;
+    double commissionBps;
+    double slippageBps;
+    double positionSize;
+
+    Mode positionMode;
+};

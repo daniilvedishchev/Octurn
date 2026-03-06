bed7e52060c7cbf55341ea07f17d36e19255565c
daniilvedishchev <daniil.vedishchev@gmail.com>
2026-03-06 23:53:57 +0100

    upd: added trade exec. logic

--- backtester/backtesterCore.cpp
+++ backtester/backtesterCore.cpp
@@ -1,14 +1,18 @@
 #include "backtesterCore.hpp"
 
-backtesterCore::backtesterCore(std::unordered_map<std::string, AnyValue>& data,config& cfg): data_(std::move(data)) ,cfg_(std::move(cfg)) {};
+backtesterCore::backtesterCore(std::unordered_map<std::string, AnyValue>& data,config& cfg): data_(std::move(data)), cfg_(std::move(cfg)) {
+    timestampVec_ = *std::get_if<std::vector<std::string>>(&data_["timestamp"]);
+    maxSize_ = timestampVec_.size();
+    history_.reserve(maxSize_);
+};
 
 trade::trade(std::string& ticker) : ticker(std::move(ticker)){};
 
-std::string backtesterCore::idx2stamp(int& idx){
-    return (*std::get_if<std::vector<std::string>>(&data_["timestamp"]))[static_cast<int>(idx)];
+std::string backtesterCore::idx2stamp(size_t& idx){
+    return timestampVec_[idx];
 };
 
-void backtesterCore::execute(std::string& ticker,std::vector<AnyValue>& entries,std::vector<AnyValue>& exits){
+void backtesterCore::execute(std::string& ticker,std::vector<bool>& entries,std::vector<bool>& exits){
 
     bool enteredTrade{false};
     trade newtrade(ticker);
@@ -16,14 +20,14 @@ void backtesterCore::execute(std::string& ticker,std::vector<AnyValue>& entries,
     size_t vectSize{entries.size()};
     for (size_t i{0}; i < vectSize; i++){
         if (!enteredTrade){
-            if (*std::get_if<bool>(&entries[i]) == true && *std::get_if<bool>(&exits[i]) == false){
+            if (entries[i] == true && exits[i] == false){
                 enteredTrade = true;
-                newtrade.timestamp.entryIdx = static_cast<int>(i);
+                newtrade.timestamp.entryIdx = i;
                 newtrade.timestamp.entryTimestamp = idx2stamp(newtrade.timestamp.entryIdx);
             }
         } else {
-            if (*std::get_if<bool>(&exits[i]) == true){
-                newtrade.timestamp.exitIdx = static_cast<int>(i);
+            if (exits[i] == true){
+                newtrade.timestamp.exitIdx = i;
                 newtrade.timestamp.exitTimestamp = idx2stamp(newtrade.timestamp.exitIdx);
                 history_.push_back(newtrade);
                 enteredTrade = false;
--- backtester/backtesterCore.hpp
+++ backtester/backtesterCore.hpp
@@ -1,10 +1,18 @@
 #include <unordered_map>
 #include "interpreter/Interpreter.hpp"
 
+struct timestamp {
+    std::string entryTimestamp;
+    size_t entryIdx;
+    std::string exitTimestamp;
+    size_t exitIdx;
+};
 
 struct trade {
+
     std::string ticker;
     timestamp timestamp;
+
     double avgExecutionPrice;
     double stopLoss;
     double positionSize;
@@ -12,20 +20,15 @@ struct trade {
     trade(std::string& ticker);
 };
 
-struct timestamp {
-    std::string entryTimestamp;
-    int entryIdx;
-    std::string exitTimestamp;
-    int exitIdx;
-};
-
 class backtesterCore {
     private:
         std::unordered_map<std::string, AnyValue> data_;
         config cfg_;
         std::vector<trade> history_;
+        std::vector<std::string> timestampVec_;
+        size_t maxSize_;
     public:
         backtesterCore(std::unordered_map<std::string, AnyValue>& data,config& cfg);
-        std::string idx2stamp(int& idx);
-        void execute(std::string& ticker,std::vector<AnyValue>& entries,std::vector<AnyValue>& exits);
+        std::string idx2stamp(size_t& idx);
+        void execute(std::string& ticker,std::vector<bool>& entries,std::vector<bool>& exits);
 };

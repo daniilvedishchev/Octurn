3a092a1946ecaba7afa1763c8c4cc9f228b67a40
daniilvedishchev <daniil.vedishchev@gmail.com>
2026-04-27 18:31:32 +0200

    upd: configSetters generic fn

--- dsl/errors/Errors.hpp
+++ dsl/errors/Errors.hpp
@@ -1,8 +1,14 @@
 #pragma once
 #include <stdexcept>
 #include <string>
+#include <unordered_map>
 
 #include "dsl/token/Token.hpp"
+#include "dsl/token/TokenTypes.hpp"
+
+struct Error : public std::runtime_error {
+    
+}
 
 struct SessionError : public std::runtime_error {
     SessionError(Token& token) : std::runtime_error("Invalid session type: " + token.lexeme + " at "
@@ -12,4 +18,9 @@ struct SessionError : public std::runtime_error {
 struct ModeError : public std::runtime_error {
     ModeError(Token& token) : std::runtime_error("Invalid mode type: " + token.lexeme + " at "
     + std::to_string(token.line) + " : " + std::to_string(token.col)){}
+};
+
+struct SlippageError : public std::runtime_error {
+    SlippageError(Token& token) : std::runtime_error("Invalid slippage type: " + token.lexeme + " at "
+    + std::to_string(token.line) + " : " + std::to_string(token.col) + "available options: [pessimistic, normal, optimistic]"){}
 };
--- dsl/parser/config/configSetters.hpp
+++ dsl/parser/config/configSetters.hpp
@@ -1,24 +1,27 @@
 #include <unordered_map>
+
 #include "dsl/token/TokenTypes.hpp"
 #include "dsl/token/Token.hpp"
-#include "namespace/namespace.hpp"
 #include "dsl/fields/session/sessionByName.hpp"
 #include "dsl/fields/mode/modeByName.hpp"
+#include "dsl/fields/slippage/slippageByName.hpp"
 #include "dsl/errors/Errors.hpp"
 
-static const std::unordered_map<tokenType,Octurn::setter> configSetterMap = {
-    {tokenType::_session, [](Token& token, config& cfg){
-        auto it = sessionByName.find(token.lexeme);
-        if (!(it != sessionByName.end())){
-            throw SessionError(token);
+#include "namespace/namespace.hpp"
+
+template <class Error, class Dict, class Field>
+auto lookupSetter(const Dict& dictName, Field config::*member){
+    return [&dictName, member](Token& token, config& cfg){
+        auto it = dictName.find(token.lexeme);
+        if (it == dictName.end()){
+            throw Error(token);
         }
-        cfg.session = it->second;
-    }},
-    {tokenType::_mode, [](Token& token, config& cfg){
-        auto it = modeByName.find(token.lexeme);
-        if (!(it != modeByName.end())){
-            throw ModeError(token);
-        }
-        cfg.mode = it->second;
-    }}
+        cfg.*member = it->second;
+    };
 }
+
+static const std::unordered_map<tokenType,Octurn::setter> configSetterMap = {
+    {tokenType::_session, lookupSetter<SessionError>(sessionByName, &config::session)},
+    {tokenType::_mode, lookupSetter<ModeError>(modeByName, &config::mode)},
+    {tokenType::_slippage, lookupSetter<SlippageError>(slippageByName, &config::slippage)}
+};
